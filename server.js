3339dd215cf03a48ae9707bef1d6baed6f7a3f1f
Ruhullah517 <111290404+Ruhullah517@users.noreply.github.com>
2024-10-29 15:28:19 +0500

    added app.get("/") in  server.js

--- server.js
+++ server.js
@@ -179,5 +179,7 @@ app.get('/accounts/:accountId/expenses', (req, res) => {
         res.status(200).send(expenses);
     });
 });
-
+app.get('/', (req,res)=>{
+    res.status(200).send("backend is running")
+})
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
